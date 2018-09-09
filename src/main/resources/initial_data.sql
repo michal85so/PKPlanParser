4233bb6ccbba808fcdf3ab5c122ff36c3e6d1ed3
Michał Policht <michal85so@gmail.com>
2018-09-09 14:04:08 +0200

    - refactor

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,5 +1,3 @@
-insert into Lecturer(id, name, shortcut) values(1, 'Jerzy Raszka', 'JR');
-insert into Lecturer(id, name, shortcut, title) values(2, 'S Fialko', 'SF', "Dr hab. inż.");
 insert into Lecture_type(id, name, shortcut) values(1, 'Wykład', 'Wyk');
 insert into Lecture_type(id, name, shortcut) values(2, 'Ćwiczenia', 'Ćw');
 insert into Lecture_type(id, name, shortcut) values(3, 'Laboratorium', 'L');
